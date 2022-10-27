134dbeef68fc6a1ac996bdd33ddeb46ae29d9240
User <rut99656@gmail.com>
2022-10-27 21:10:16 +0300

    inital code + welcome function

---
+++ Targil0/8222.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome8222();
+            welcome0227();
+            Console.ReadKey();
+        }
+        static partial void welcome0227();
+        private static void welcome8222()
+        {
+            Console.WriteLine("Enter your Name: ");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application",name);
+        }
+
+    }
+}
