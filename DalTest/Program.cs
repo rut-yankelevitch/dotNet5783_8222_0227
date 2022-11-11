513a1121ef0473f1d485c770a55a1ed0b7f0a755
User <rut99656@gmail.com>
2022-11-11 02:36:50 +0200

    עוד עדכון

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,18 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 namespace DalTest {
     Enum Entities { EXIT,PRODUCT,ORDER,ORDER_ITEM};
-    Enum Options { ADD,GET,GET_ALL,UPDATE,DELETE};
+    Enum Options { ADD=1,GET,GET_ALL,UPDATE,DELETE};
 
     public class Program { 
         private Product p= new Product ();
         private Order o= new Order ();
         private OrderItem oI= new OrderItem ();
         public void ProductEntities()
-        {   string op=Console.ReadLine ();
+        { 
+            Console.WriteLine("Product menu: \n 1-add \n 2-get all \n 3- get by id \n 4- update \n 5- delete");
+            string op = Console.ReadLine ();
             Options o=(Options)int.Parse(op)
+            string s;
             switch (o)
 	        {
                 case ADD:
+                    {
+                        Console.WriteLine("enter product-name: ");
+                        Console.ReadLine(p.Name);
+                        Console.WriteLine("enter product-category: ");
+                        Console.ReadLine(p.Category);
+                        Console.WriteLine("enter product-price: ");
+                        Console.ReadLine(p.Price);
+                        Console.WriteLine("enter amount in stoke: ");
+                        Console.ReadLine(p.inStoke);
+                    }
                     break;
                 case GET:
                     break;
