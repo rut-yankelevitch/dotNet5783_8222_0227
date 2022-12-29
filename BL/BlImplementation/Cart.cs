1b2e71aab22f400fd3c17b287657da9c45592783
User <rut99656@gmail.com>
2022-12-29 17:03:53 +0200

    cart linq

--- BL/BlImplementation/Cart.cs
+++ BL/BlImplementation/Cart.cs
@@ -72,10 +72,10 @@ internal class Cart : ICart
     /// <exception cref="BO.BLDoesNotExistException"></exception>
     public BO.Cart UpdateProductAmountInCart(BO.Cart cart, int idProduct, int amount)
     {
-        bool flag = false;
         try
         {
             DO.Product product = new DO.Product();
+            BO.OrderItem newOrderItem = new BO.OrderItem();
             product = dal.Product.GetByCondition(product2=>product2?.ID==idProduct);
 
             if (product.InStock < amount)
@@ -88,16 +88,32 @@ internal class Cart : ICart
                 var sum = cart.Items.Where(item => item?.ProductID == idProduct).Sum(item => item?.TotalPrice)??
                     throw new BLImpossibleActionException("This item is not in the cart");
                 cart.TotalPrice -= (double)sum!;
-     
-                if (amount == 0)
+                if (amount > 0) 
                 {
-                    cart.Items.RemoveAll(item => item?.ProductID == idProduct);
-                }
-                else
-                {
-              //cart.Items.Select(item => item.Amount = amount &&item.TotalPrice=item.Price*amount);
-                }
-                
+                    var newItem = cart.Items.FirstOrDefault(item=> item?.ProductID == idProduct);
+                    if(newItem != null)
+                    {
+                        newOrderItem = newItem;
+                        newOrderItem.Amount = amount;
+                        newOrderItem.TotalPrice= amount*newItem.Price;
+                        cart.TotalPrice += newOrderItem.TotalPrice;
+                    }
+                 }
+                cart.Items.RemoveAll(item => item?.ProductID == idProduct);
+                if (amount > 0)
+                    cart.Items.Add(newOrderItem);
+
+
+
+
+                //if (amount == 0)
+                //{
+                //}
+                //else
+                //{
+                //  //var a= cart.Items.Where(item => item?.ProductID == idProduct).Select(item => item.ID = amount );
+                //}
+
 
                 //foreach (BO.OrderItem orderItem in cart.Items)
                 //{
@@ -126,18 +142,10 @@ internal class Cart : ICart
                 //        }
                 //    }
                 //}
-                
-                   
-                    
-                    
-
-
-
-
-                if (flag == false)
-                {
-                    throw new BLImpossibleActionException("This item is not in the cart");
-                }
+                //if (flag == false)
+                //{
+                //    throw new BLImpossibleActionException("This item is not in the cart");
+                //}
             }
             else
                 throw new BLImpossibleActionException("There are no items in the cart");
@@ -164,7 +172,7 @@ internal class Cart : ICart
         try
         {
             DO.Order order = new DO.Order();
-            DO.Product product = new DO.Product();
+            //DO.Product product = new DO.Product();
 
             if (cart.CustomerName == "" || cart.CustomerEmail == "" || cart.CustomerAddress == "")
             {
@@ -177,45 +185,56 @@ internal class Cart : ICart
             order.ShipDate = null;
             order.DeliveryrDate =null;
             int id = dal.Order.Add(order);
+            var a = from ordItem in cart.Items
+                    let product = dal.Product.GetByCondition(product2 => product2?.ID == ordItem.ProductID)
+                    where product.InStock >= ordItem.Amount
+                    select new
+                    {
+                        orderItemId =dal.OrderItem.Add(new DO.OrderItem { OrderID = id, ProductID = ordItem.ProductID, Amount = ordItem.Amount, Price = ordItem.Price })
+                        /*להוסיף זריקה*/,
+                        product2 = new DO.Product { Price = product.Price, Category = product.Category, ID = product.ID, Name = product.Name, InStock = (product.InStock) - ordItem.Amount }
+                    };
+             a.ToList().ForEach(a => dal.Product.Update(a.product2));/*להוסיף זריקה*/
 
-            foreach (BO.OrderItem orderItem in cart.Items)
-            {
-                try
-                {
-                    product = dal.Product.GetByCondition(product2=>product2?.ID==orderItem.ProductID);
-                }
-                catch (DO.DalDoesNotExistException ex)
-                {
-                    throw new BO.BLDoesNotExistException("product dosent exsit", ex);
-                }
-                if (orderItem?.Amount <= 0)
-                    throw new BO.BLImpossibleActionException("invalid amount");
-                if (product.InStock < orderItem.Amount)
-                    throw new BO.BLImpossibleActionException("amount not in stock ");
 
-                DO.OrderItem orderItem1 = new DO.OrderItem();
-                orderItem1.OrderID = id;
-                orderItem1.ProductID = orderItem.ProductID;
-                orderItem1.Amount = orderItem.Amount;
-                orderItem1.Price = orderItem.Price;
-                product.InStock -= orderItem.Amount;
-                try
-                {
-                    dal.Product.Update(product);
-                }
-                catch (DO.DalDoesNotExistException ex)
-                {
-                    throw new BO.BLDoesNotExistException("product dosent exsit", ex);
-                }
-                try
-                {
-                    dal.OrderItem.Add(orderItem1);
-                }
-                catch (DO.DalDoesNotExistException ex)
-                {
-                    throw new BO.BLDoesNotExistException($"{ex.EntityName} dosent exsit", ex);
-                }
-            }
+            //foreach (BO.OrderItem orderItem in cart.Items)
+            //{
+            //    try
+            //    {
+            //        product = dal.Product.GetByCondition(product2=>product2?.ID==orderItem.ProductID);
+            //    }
+            //    catch (DO.DalDoesNotExistException ex)
+            //    {
+            //        throw new BO.BLDoesNotExistException("product dosent exsit", ex);
+            //    }
+            //////    if (orderItem?.Amount <= 0)
+            //////        throw new BO.BLImpossibleActionException("invalid amount");
+            //    if (product.InStock < orderItem.Amount)
+            //        throw new BO.BLImpossibleActionException("amount not in stock ");
+
+            //    DO.OrderItem orderItem1 = new DO.OrderItem();
+            //    orderItem1.OrderID = id;
+            //    orderItem1.ProductID = orderItem.ProductID;
+            //    orderItem1.Amount = orderItem.Amount;
+            //    orderItem1.Price = orderItem.Price;
+            //    product.InStock -= orderItem.Amount;
+            //    try
+            //    {
+            //        dal.Product.Update(product);
+            //    }
+            //    catch (DO.DalDoesNotExistException ex)
+            //    {
+            //        throw new BO.BLDoesNotExistException("product dosent exsit", ex);
+            //    }
+            //    try
+            //    {
+            //        dal.OrderItem.Add(orderItem1);
+            //    }
+            //    catch (DO.DalDoesNotExistException ex)
+            //    {
+            //        throw new BO.BLDoesNotExistException($"{ex.EntityName} dosent exsit", ex);
+            //    }
+            //}
         }
         catch (DO.DalDoesNotExistException ex)
         {
