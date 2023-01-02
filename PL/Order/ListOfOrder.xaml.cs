c1c164b29d6c626159d1611f6191ffbe14036f90
User <rut99656@gmail.com>
2023-01-03 01:48:14 +0200

    order - Template change

--- PL/Order/ListOfOrder.xaml.cs
+++ PL/Order/ListOfOrder.xaml.cs
@@ -1,46 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+//public partial class ProductForListWindow : Window
+//{
+//    //A private variable to access the logic layer
+//    BlApi.IBl bl = BlApi.Factory.Get();
+
+//    public ObservableCollection<BO.ProductForList?> Products
+//    {
+//        get { return (ObservableCollection<BO.ProductForList?>)GetValue(ProductsProperty); }
+//        set { SetValue(ProductsProperty, value); }
+//    }
+
+//    // Using a DependencyProperty as the backing store for Products.  This enables animation, styling, binding, etc...
+//    public static readonly DependencyProperty ProductsProperty =
+//        DependencyProperty.Register("Products", typeof(ObservableCollection<BO.ProductForList?>), typeof(ProductForListWindow), new PropertyMetadata(null));
+
+//    /// <summary>
+//    /// A constructor action that initializes the controls
+//    /// </summary>
+//    public ProductForListWindow()
+//    {
+//        InitializeComponent();
+
+//        //Default all products
+//        cmxCategorySelector.SelectedIndex = 5;
+
+//        //Request the logical layer to bring all the products
+//        // ProductListview.ItemsSource = bl.product.GetListOfProducts();
+
+//        var temp = bl.product.GetListOfProducts();
+//        Products = temp == null ? new() : new(temp);
+//        //Put all the categories
+//        cmxCategorySelector.ItemsSource = Enum.GetValues(typeof(BO.Category));
+//    }
+
+
+using System;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace PL.Order
-{
+
+namespace PL.Order;
+
     /// <summary>
     /// Interaction logic for ListOfOrder.xaml
     /// </summary>
     public partial class ListOfOrder : Window
     {
         private BlApi.IBl bl = BlApi.Factory.Get();
+        public ObservableCollection<BO.OrderForList?> Orders
+    {
+            get { return (ObservableCollection<BO.OrderForList?>)GetValue(OrdersProperty); }
+            set { SetValue(OrdersProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for orders.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OrdersProperty =
+            DependencyProperty.Register("orders", typeof(ObservableCollection<BO.OrderForList?>), typeof(ListOfOrder), new PropertyMetadata(null));
+
         //private ObservableCollection<BO.OrderForList?>? orders= new ObservableCollection<BO.OrderForList?>();
         public ListOfOrder()
         {
             InitializeComponent();
-            var tmpOrder= bl.Order.GetOrderList();
-            OrderForListData orders = (tmpOrder == null) ? new() : new(tmpOrder) ;
-
-            listViewOrders.DataContext = orders;
+            var temp = bl.Order.GetOrderList();
+            Orders = (temp == null) ? new() : new(temp);
         }
     }
-    public class OrderForListData: DependencyObject
-    {
-        public ObservableCollection<BO.OrderForList> orders
-        {
-            get { return (ObservableCollection<BO.OrderForList>)GetValue(ordersProperty); }
-            set { SetValue(ordersProperty, value); }
-        }
 
-        // Using a DependencyProperty as the backing store for orders.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ordersProperty =
-            DependencyProperty.Register("orders", typeof(ObservableCollection<BO.OrderForList>), typeof(OrderForListData), new PropertyMetadata(0));
-    }
-}
