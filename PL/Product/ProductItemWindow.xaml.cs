0dc89304cc43102374071c444b852bfb31d723d8
RacheliShafran <r0527197777@gmail.com>
2023-02-05 20:16:20 +0200

    mnm

---
+++ PL/Product/ProductItemWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.Product
+{
+    /// <summary>
+    /// Interaction logic for ProductItemWindow.xaml
+    /// </summary>
+    public partial class ProductItemWindow : Window
+    {
+        public ProductItemWindow(int id,BO.Cart? cart)
+        {
+            InitializeComponent();
+        }
+    }
+}
